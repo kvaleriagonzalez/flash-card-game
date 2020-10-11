870ea2efc8e969f3fc56c4f006729c72f2b53fd5
karen <k.valeria.gonzalez@gmail.com>
2020-10-11 18:46:41 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    // card flip effect was created by Hirok Banik
+
+    let stack = document.querySelector(".stack");
+
+    [...stack.children].reverse().forEach(i => stack.append(i));
+
+    stack.addEventListener("click", swap);
+
+    function swap(e) {
+        let card = document.querySelector(".card:last-child");
+        if (e.target !== card) return;
+        card.style.animation = "swap 700ms forwards";
+        $(".wrong").show();
+        $('p').empty();
+        setTimeout(() => {
+            card.style.animation = "";
+            stack.prepend(card);
+        }, 700);
+    }
+
+
+    $('.answer').on('click', function () {
+        $('p').text("Correct answer");
+
+    });
+
+    $('.answer').on('click', event => {
+        $(".wrong").hide();
+
+    })
+
+    $('.wrong').on('click', function () {
+        $(this).css('backgroundColor', 'rgba(255, 117, 107, 0.68)')
+        alert(`Wrong answer`)
+
+    });
+
+    const juno = 'Created at Juno College';
+    console.log(`${juno}.`)
+
+});
